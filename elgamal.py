c50bcf51c347fbab8ce68c750b1a5be4b83d01dc
thomascox <coxt@seas.upenn.edu>
2022-09-18 19:18:08 -0400

    commit

--- elgamal.py
+++ elgamal.py
@@ -14,13 +14,16 @@ def keygen():
 def encrypt(pk,m):
     q = (p - 1) / 2
     r = random.randint(1, q)
-    #hr = pow(pk,r,m)
 
     c1 = pow(g,r,p)
     c2 = (pow(pk,r,p)*m)%p
     return [c1,c2]
 
 def decrypt(sk,c):
-    #print(c[0])
-    m = (c[1])/ (pow(c[0],sk,p))
+
+    h = pow(g,sk,p)
+
+    j = pow(c[0],sk,p)
+    k = pow(j,-1,p)
+    m = (c[1]*k)%p
     return m
